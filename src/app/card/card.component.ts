0a16aaa88ca0ecff89a6ff671b8b4ea7b0afcf75
yushachkov <a_yushachkov@mail.ru>
2022-03-24 16:37:26 +0300

    использование Байнинга, setTimeout

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core'; //Импортирую что-то из angular/core'
-
+import { Component, OnInit} from '@angular/core'; //Импортирую что-то из angular/core'
 
 @Component({
     selector: 'app-card',//создаем selector с app, чтобы мы могли различать где наши компоненты, а где чужие, спросить что такое селектор
@@ -7,21 +6,18 @@ import { Component } from '@angular/core'; //Импортирую что-то и
     styleUrls: ['./card.component.scss'],
 
 })
-export class CardComponent {
-    title : string= 'My card Title'
+export class CardComponent implements OnInit {
+    title = 'My card Title'
     text: string = 'My sample text'
 
     imgUrl: string = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/21_Angular_logo_logos-512.png'
 
-    number = 42
+    disabled = false;
 
-    array = [1, 1, 2, 3, 5, 8, 13]
-
-    obj = {name: 'Anton', info: {ade: 20, job: 'Frontend'}}
-
-    getInfo(): string{
-        return 'This is my info:'
+    ngOnInit(){
+        setTimeout(() => {
+            this.imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png'
+            this.disabled = true
+        },3000)
     }
-
-
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,11 +6,9 @@ import { Component} from '@angular/core';
     <div>
     <h1>Card Components</h1>
     <div class="card">
-<h2>Card Component</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, molestias.</p>
-</div>
-
-
+    <h2>Card Component</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, molestias.</p>
+    </div>
     </div>`,
     styles: [`
     .card{

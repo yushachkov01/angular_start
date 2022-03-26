5be592bd309f0cbabc0a19a5e6e4d8d922e2221e
yushachkov <a_yushachkov@mail.ru>
2022-03-27 00:39:48 +0300

    Two-way binding (двустороннее связывание) input c title

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,20 +4,24 @@ import { Component, OnInit} from '@angular/core'; //Импортирую что-
     selector: 'app-card',//создаем selector с app, чтобы мы могли различать где наши компоненты, а где чужие, спросить что такое селектор
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss'],
-
 })
 export class CardComponent implements OnInit {
     title = 'My card Title'
     text: string = 'My sample text'
 
-    imgUrl: string = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/21_Angular_logo_logos-512.png'
-
     disabled = false;
 
-    ngOnInit(){
-        setTimeout(() => {
-            this.imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png'
-            this.disabled = true
-        },3000)
+    ngOnInit(){}
+
+    changeTitle(){
+        this.title = 'Title has been changed!'
     }
+
+
+    inputHandler(event: any){
+        const value = event.target.value;
+        this.title = value
+     }
+ 
+ 
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,17 +10,7 @@ import { Component} from '@angular/core';
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, molestias.</p>
     </div>
     </div>`,
-    styles: [`
-    .card{
-        padding: .5rem 1rem;
-        border: 1px dashed #ccc;
-        margin-bottom: .1rem;
-    
-        h2{
-            margin-bottom: .5rem;
-        }
-    }
-    `]
+
 })
 export class FormComponent  {
 

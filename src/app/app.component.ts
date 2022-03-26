79327d5e6a9cf1a15914b2d5079023c1ec2bef0b
yushachkov <a_yushachkov@mail.ru>
2022-03-27 02:48:47 +0300

    Скрываем, либо отображаем все карточки, которые у нас есть

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-basics';
+  toogle = true
+
+  toogleCards(){
+    this.toogle = !this.toogle
+  }
 }

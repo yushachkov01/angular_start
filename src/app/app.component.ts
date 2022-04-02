67537f3716590e2acef59921c6ce42abc6a9533f
yushachkov <a_yushachkov@mail.ru>
2022-04-03 00:20:33 +0300

    Kard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ export class AppComponent {
   toogle = true
 
   cards: Card[] = [
-    {title: 'Card 1', text:'This is card number 1'},
-    {title: 'This is Card 2', text:'This is card number 2'},
-    {title: 'Last Card!', text:'This is card number 3'}
+    {title: 'Card Name', text:'0000 0000 0000 0000'},
+    {title: 'This is Card 2', text:'1233 7474 8594 824O'},
+    {title: 'Last Card!', text:'7284 8493 3843 3949'}
   ]
 
   toogleCards(){
